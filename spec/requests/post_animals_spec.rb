5d0ec3e6d8cf99a0f304eaf54a21200dd1657ce5
Lara Bjork <lara.m.bjork@gmail.com>
2020-01-31 12:48:03 -0800

    add passing spec for post_animal

---
+++ spec/requests/post_animals_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "post an animal route", :type => :request do
+
+  before do
+    post '/animals', params: { :name => 'ramona', :date_available => '2019-12-31', :breed => 'domestic long hair', :sex => 'female', :color => 'brown tabby',  :age => 6,  :weight => 7,  :location => 'Orion', :kennel => 23,  :profile => 'completely lovable',  :picture => 'insert url here' }
+  end
+
+  it 'returns the animal name' do
+    expect(JSON.parse(response.body)['name']).to eq('ramona')
+  end
+
+  it 'returns the date available' do
+    expect(JSON.parse(response.body)['date_available']).to eq('2019-12-31')
+  end
+
+  it 'returns the animal weight' do
+    expect(JSON.parse(response.body)['weight']).to eq(7)
+  end
+
+  it 'returns a created status' do
+    expect(response).to have_http_status(:created)
+  end
+end
