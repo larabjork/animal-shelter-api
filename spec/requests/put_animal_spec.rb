baa5987dd9c3cd5b4b16f8e25f6fa9d538c66be2
Lara Bjork <lara.m.bjork@gmail.com>
2020-02-02 11:18:39 -0800

    add passing code for put animal

--- spec/requests/put_animal_spec.rb
+++ spec/requests/put_animal_spec.rb
@@ -1,31 +1,17 @@
 require 'rails_helper'
 
 describe "update an animal record", :type => :request do
-let!(:animals) { FactoryBot.create_list(:animal, 20)}
+let!(:animals) { FactoryBot.create_list(:animal, 1)}
 
   before do
-    put '/animals/46', params: { :name => 'ramona', :date_available => '2019-12-28', :animal_type => 'domestic medium hair', :sex => 'female', :color => 'brown tabby',  :age => 6,  :weight => 8,  :location => 'Orion', :kennel => 23,  :profile => 'completely lovable',  :picture => 'url goes here' }
+    put "/animals/#{Animal.first.id}", params: { :name => 'ramona', :date_available => '2019-12-28', :animal_type => 'domestic medium hair', :sex => 'female', :color => 'brown tabby',  :age => 6,  :weight => 8,  :location => 'Orion', :kennel => 23,  :profile => 'completely lovable',  :picture => 'url goes here' }
   end
 
-  it 'returns the animal name' do
-    expect(JSON.parse(response.body)['name']).to eq('ramona')
-  end
-
-  it 'returns the date available' do
-    expect(JSON.parse(response.body)['date_available']).to eq('2019-12-28')
-  end
-
-  it 'returns the animal weight' do
-    expect(JSON.parse(response.body)['weight']).to eq(8)
+  it 'returns a successful update message' do
+    expect(JSON.parse(response.body)['message']).to eq('This animal has been updated successfully.')
   end
 
   it 'returns a created status' do
-    expect(response).to have_http_status(:created)
+    expect(response).to have_http_status(:success)
   end
 end
-
-
-
-# do
-#   post '/animals', params: { :name => 'ramona', :date_available => '2019-12-31', :animal_type => 'domestic long hair', :sex => 'female', :color => 'brown tabby',  :age => 6,  :weight => 7,  :location => 'Orion', :kennel => 23,  :profile => 'completely lovable',  :picture => 'insert url here' }
-#   binding.pry
