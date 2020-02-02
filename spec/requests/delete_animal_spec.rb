bbc76ee58228dcb6671092a042d9743953bb3cba
Lara Bjork <lara.m.bjork@gmail.com>
2020-02-02 13:24:13 -0800

    add additional passing test for delete animal but no id provided

--- spec/requests/delete_animal_spec.rb
+++ spec/requests/delete_animal_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
 describe "delete an animal record", :type => :request do
-let!(:animals) { FactoryBot.create_list(:animal, 1)}
+  let!(:animals) { FactoryBot.create_list(:animal, 1)}
 
-  before do
-    delete "/animals/#{Animal.first.id}"
+  context "when an animal record has been deleted" do
+    before do
+      delete "/animals/#{Animal.first.id}"
+    end
+
+    it 'returns a successful update message' do
+      expect(JSON.parse(response.body)['message']).to eq('This animal has been deleted successfully.')
+    end
+
+    it 'returns a message that animal has been deleted' do
+      expect(response).to have_http_status(:success)
+    end
   end
 
-  it 'returns a successful update message' do
-    expect(JSON.parse(response.body)['message']).to eq('This animal has been deleted successfully.')
+  context 'handle exception when no animal id provided' do
+    before { delete '/animals/:potato'}
+    it 'returns status code 404' do
+      expect(response).to have_http_status(:not_found)
+    end
   end
 
-  it 'returns a message that animal has been deleted' do
-    expect(response).to have_http_status(:success)
-  end
 end
